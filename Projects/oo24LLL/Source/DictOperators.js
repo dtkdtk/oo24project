2fd9f0e6b59d33c37b31f0712986e65e38ff6175
dtkdtk <dtkdtk0.mail@gmail.com>
2025-02-16 23:50:09 +0300

    Some changes

--- Projects/oo24LLL/Source/DictOperators.js
+++ Projects/oo24LLL/Source/DictOperators.js
@@ -7,6 +7,9 @@ import * as aux from "./aAux.js";
  * @type {Dictionary}
  */
 export default new Map(Object.entries({
+
+  /* АРИФМЕТИКА */
+
   "[+]": (S) => {
     aux.AssertStackLength(S, 2);
     const B = aux.Pop_Number(S);
@@ -53,10 +56,93 @@ export default new Map(Object.entries({
     const X = aux.Pop_Number(S);
     S.Stack.push(X - 1);
   },
+
+  /* СРАВНЕНИЕ */
+
   "[==]": (S) => {
     aux.AssertStackLength(S, 2);
     const B = aux.Pop_Number(S);
     const A = aux.Pop_Number(S);
     S.Stack.push(A == B ? 1 : 0);
   },
+  "[!=]": (S) => {
+    aux.AssertStackLength(S, 2);
+    const B = aux.Pop_Number(S);
+    const A = aux.Pop_Number(S);
+    S.Stack.push(A != B ? 1 : 0);
+  },
+  "[>]": (S) => {
+    aux.AssertStackLength(S, 2);
+    const B = aux.Pop_Number(S);
+    const A = aux.Pop_Number(S);
+    S.Stack.push(A > B ? 1 : 0);
+  },
+  "[>=]": (S) => {
+    aux.AssertStackLength(S, 2);
+    const B = aux.Pop_Number(S);
+    const A = aux.Pop_Number(S);
+    S.Stack.push(A >= B ? 1 : 0);
+  },
+  "[<]": (S) => {
+    aux.AssertStackLength(S, 2);
+    const B = aux.Pop_Number(S);
+    const A = aux.Pop_Number(S);
+    S.Stack.push(A < B ? 1 : 0);
+  },
+  "[<=]": (S) => {
+    aux.AssertStackLength(S, 2);
+    const B = aux.Pop_Number(S);
+    const A = aux.Pop_Number(S);
+    S.Stack.push(A <= B ? 1 : 0);
+  },
+
+  /* ПОБИТОВЫЕ/ЛОГИЧЕСКИЕ */
+
+  and: (S) => {
+    aux.AssertStackLength(S, 2);
+    const B = aux.Pop_Number(S);
+    const A = aux.Pop_Number(S);
+    S.Stack.push(A & B);
+  },
+  or: (S) => {
+    aux.AssertStackLength(S, 2);
+    const B = aux.Pop_Number(S);
+    const A = aux.Pop_Number(S);
+    S.Stack.push(A | B);
+  },
+  not: (S) => {
+    aux.AssertStackLength(S, 1);
+    const X = aux.Pop_Number(S);
+    S.Stack.push(X ? 0 : 1);
+  },
+  bitxor: (S) => {
+    aux.AssertStackLength(S, 2);
+    const B = aux.Pop_Number(S);
+    const A = aux.Pop_Number(S);
+    S.Stack.push(A ^ B);
+  },
+  bitnot: (S) => {
+    aux.AssertStackLength(S, 1);
+    const X = aux.Pop_Number(S);
+    S.Stack.push(~ X);
+  },
+  "[>>]": (S) => {
+    aux.AssertStackLength(S, 2);
+    const B = aux.Pop_Number(S);
+    const A = aux.Pop_Number(S);
+    S.Stack.push(A >> B);
+  },
+  "[>>>]": (S) => {
+    aux.AssertStackLength(S, 2);
+    const B = aux.Pop_Number(S);
+    const A = aux.Pop_Number(S);
+    S.Stack.push(A >>> B);
+  },
+  "[<<]": (S) => {
+    aux.AssertStackLength(S, 2);
+    const B = aux.Pop_Number(S);
+    const A = aux.Pop_Number(S);
+    S.Stack.push(A << B);
+  },
+
 }));
